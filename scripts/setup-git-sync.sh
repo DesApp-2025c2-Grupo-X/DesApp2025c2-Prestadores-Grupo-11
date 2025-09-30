3ff192dc0d4293e2140ef2fac2867f79bb6a7e97
Erika-Campos-V <erikarocio.camposvera@estudiantes.unahur.edu.ar>
2025-09-30 15:35:43 -0300

    refactor sync.sh

--- scripts/setup-git-sync.sh
+++ scripts/setup-git-sync.sh
@@ -5,13 +5,14 @@ echo "⚙️ Instalando alias global 'git sync' (fetch + crear ramas locales, mo
 
 git config --global alias.sync '!f() { \
     git fetch --all && \
-    for branch in $(git branch -r | grep -v "->"); do \
+    for branch in $(git branch -r | awk "!/HEAD/ {print}"); do \
         local_branch=${branch#origin/}; \
         if ! git show-ref --verify --quiet refs/heads/"$local_branch"; then \
             git branch --track "$local_branch" "$branch" 2>/dev/null || true; \
         fi; \
     done; \
 }; f'
+
 
 
 echo "✅ Alias 'git sync' instalado."
