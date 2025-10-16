7b4b3e3ea462a00a4e5cac0849608a8bfcd4bbed
juancamargo02 <juaninachocamargo@hotmail.com>
2025-10-16 16:43:28 -0300

    Antecedentes medicos agregado, faltaria agregarle la logica al filtro de notas

--- Prestadores-Grupo11/src/pages/HistorialClinico.jsx
+++ Prestadores-Grupo11/src/pages/HistorialClinico.jsx
@@ -14,6 +14,7 @@ export default function HistorialClinico() {
   const [afiliado, setAfiliado] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [filtroNotas, setFiltroNotas] = useState(false);
 
   // Cargar datos del afiliado
   useEffect(() => {
@@ -115,7 +116,7 @@ export default function HistorialClinico() {
           >
             <div className="d-flex align-items-center gap-3">
               <Users size={40} color="var(--azul-petroleo)" />
-              <div>
+              <div style={{ borderBottom: "black 1px solid" }}>
                 <h4>{afiliado.nombre}</h4>
                 <p>
                   Edad: <strong>{afiliado.edad}</strong> | DNI:{" "}
@@ -123,59 +124,65 @@ export default function HistorialClinico() {
                 </p>
               </div>
             </div>
+            <div>
+              <h2 style={{ marginTop: "10px" }}>Antecedentes medicos</h2>
+              {afiliado.antecedentes.map((ant, idx) => (
+                <p key={idx}>{ant}</p>
+              ))}
+            </div>
+
           </motion.div>
 
-          {/*Contenedor de ultimas consultas y antecedentes*/}
-          <div>
+          {/* Tabla ultimas consultas */}
+          <motion.div
+            className="tabla-container"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            <h3>Ultimas consultas</h3>
+            
+            {/*Checkbox para filtrar entre notas propias*/}
+            <label style={{marginLeft: "20%"}}>
+              <input
+                type="checkbox"
+                checked={filtroNotas}
+                onChange={(e) => setFiltroNotas(e.target.checked)}
+              />
+              Filtrar por notas propias
+            </label>
 
-            {/* Tabla ultimas consultas */}
-            <motion.div
-              className="tabla-container"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.4 }}
-            >
-              <table className="table table-striped">
-                <thead>
-                  <tr>
-                    <th>Fecha</th>
-                    <th>Descripcion</th>
-                    <th>Especialidad</th>
-                    <th>Medico</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {afiliado.consultas.map((consulta, idx) => (
-                    <motion.tr
-                      key={idx}
-                      initial={{ opacity: 0, y: 10 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      transition={{ delay: idx * 0.05 }}
-                      whileHover={{
-                        scale: 1.02,
-                      }}
-                    >
-                      <td>{consulta.fecha}</td>
-                      <td>{consulta.descripcion}</td>
-                      <td>{consulta.especialidad}</td>
-                      <td>{consulta.medico}</td>
-                    </motion.tr>
-                  ))}
-                </tbody>
-              </table>
-            </motion.div>
-
-            {/*Antecedentes*/}
-            <motion.div
-              className="tabla-container"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.4 }}
-            >
-              <p style={{backgroundColor: "red",}}>Coso donde pruebo el coso</p>
-            </motion.div>
-          </div>
-
+            <table className="table table-striped" style={{ marginTop: "0px" }}>
+              <thead>
+                <tr>
+                  <th>Fecha</th>
+                  <th>Descripcion</th>
+                  <th>Especialidad</th>
+                  <th>Medico</th>
+                  <th>Notas</th>
+                </tr>
+              </thead>
+              <tbody>
+                {afiliado.consultas.map((consulta, idx) => (
+                  <motion.tr
+                    key={idx}
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: idx * 0.05 }}
+                    whileHover={{
+                      scale: 1.02,
+                    }}
+                  >
+                    <td>{consulta.fecha}</td>
+                    <td>{consulta.descripcion}</td>
+                    <td>{consulta.especialidad}</td>
+                    <td>{consulta.medico}</td>
+                    <td>{consulta.notas}</td>
+                  </motion.tr>
+                ))}
+              </tbody>
+            </table>
+          </motion.div>
         </div>
       </div>
     </PrestadoresLayout>
