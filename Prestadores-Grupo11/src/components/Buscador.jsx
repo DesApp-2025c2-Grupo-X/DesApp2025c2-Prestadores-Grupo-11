2adffbb3ab437c18ae9435d1da55ba583b8216b1
juancamargo02 <juaninachocamargo@hotmail.com>
2025-11-04 20:28:55 -0300

    correccion de error de tipeo

--- Prestadores-Grupo11/src/components/Buscador.jsx
+++ Prestadores-Grupo11/src/components/Buscador.jsx
@@ -102,7 +102,7 @@ export default function Buscador({
         )}
       </AnimatePresence>
       {/* === Toasts globales === */}
-      <TostContainer position="top-right" autoClose={3000} theme="colored" />
+      <ToastContainer position="top-right" autoClose={3000} theme="colored" />
     </motion.div>
   );
 }
